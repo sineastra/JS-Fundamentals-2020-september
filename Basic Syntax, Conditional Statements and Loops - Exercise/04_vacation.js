0c7bb35432b87174d903bd30fd2dfb8400c85319
Sineastra <sineastra@gmail.com>
2020-09-16 23:57:42 +0300

    added 04_vacation.js

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/04_vacation.js
@@ -0,0 +1,32 @@
+function foo(number, type, day) {
+    const types = {
+        Students: {
+            Friday: 8.45,
+            Saturday: 9.8,
+            Sunday: 10.46,
+        },
+        Business: {
+            Friday: 10.9,
+            Saturday: 15.6,
+            Sunday: 16,
+        },
+        Regular: {
+            Friday: 15,
+            Saturday: 20,
+            Sunday: 22.5,
+        },
+    }
+
+    function display(r) {
+        return `Total price: ${r.toFixed(2)}`
+    }
+    const result = types[type][day] * number
+    if (type === "Students" && number >= 30) return display(result - result * 0.15)
+    if (type === "Business" && number >= 100) return display(result - 10 * types[type][day])
+    if (type === "Business" && number >= 10 && number <= 20) return display(result - result * 0.05)
+
+    return display(result)
+}
+
+console.log(foo(30, "Students", "Sunday"))
+console.log(foo(40, "Regular", "Saturday"))
