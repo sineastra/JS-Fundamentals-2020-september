137bc72a28f432350a5ec96943f10ff8c2bf0d35
Sineastra <sineastra@gmail.com>
2020-09-16 22:41:10 +0300

    added task 3 - division

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/03_division.js
@@ -0,0 +1,12 @@
+function foo(n) {
+    let division = false
+    const divisions = [2, 3, 6, 7, 10]
+
+    divisions.forEach(x => {
+        if (n % x === 0) division = x
+    })
+
+    return division ? `The number is divisible by ${division}` : `Not divisible`
+}
+
+console.log(foo(15)) 
