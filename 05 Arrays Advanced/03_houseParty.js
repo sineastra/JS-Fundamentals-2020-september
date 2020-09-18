e6083499a624af8a0209941902ea61d84d26eeda
Sineastra <lord.of.light.0002@gmail.com>
2020-09-18 22:39:11 +0300

    added 03_houseParty

---
+++ 05 Arrays Advanced/03_houseParty.js
@@ -0,0 +1,17 @@
+function foo(arr) {
+    let going = []
+    arr.forEach(x => {
+        const data = x.split(" ")
+        if (data.length === 3) {
+            going.includes(data[0]) ? console.log(`${data[0]} is already in the list!`) : going.push(data[0])
+        } else {
+            going.includes(data[0])
+                ? (going = going.filter(x => x !== data[0]))
+                : console.log(`${data[0]} is not in the list!`)
+        }
+    })
+
+    going.forEach(x => console.log(x))
+}
+
+foo(["Tom is going!", "Annie is going!", "Tom is going!", "Garry is going!", "Jerry is going!"])
