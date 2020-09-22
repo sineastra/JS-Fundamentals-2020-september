cfc658842ede391303e4e50ea4fbb2ae5ae10f19
Sineastra <lord.of.light.0002@gmail.com>
2020-09-22 12:18:25 +0300

    added 06 Associative Arrays added 01_wordTracker.js

---
+++ 07 Associative Arrays/01_wordTracker.js
@@ -0,0 +1,12 @@
+function foo(arr) {
+    const words = new Map(
+        arr
+            .shift()
+            .split(" ")
+            .map(x => [x, 0])
+    )
+    arr.forEach(x => {
+        if (words.get(x) !== undefined) words.set(x, words.get(x) + 1)
+    })
+    return [...words.entries()].sort((x, y) => y[1] - x[1]).forEach(x => console.log(`${x[0]} - ${x[1]}`))
+}
