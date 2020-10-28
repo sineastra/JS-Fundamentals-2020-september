2c00f6b6c7cfcead4b31a511f74d454000c4a266
Sineastra <lord.of.light.0002@gmail.com>
2020-10-28 23:10:11 +0200

    added 07 Mid Exam Retake - 07 April 2020 added 01 Counter Strike

---
+++ -- mid exams --/07 Mid Exam Retake - 07 April 2020/01 Counter Strike/counterStrike.js
@@ -0,0 +1,28 @@
+function foo(arr) {
+    const extractIndex = arr.findIndex(x => x === "End of battle")
+    arr = arr
+        .slice(0, extractIndex !== -1 ? extractIndex : arr.length + 1)
+        .map(Number)
+    let energy = arr.shift()
+    let wonBattles = 0
+
+    for (let i = 0; i < arr.length; i += 1) {
+        if (energy - arr[i] < 0) {
+            console.log(
+                `Not enough energy! Game ends with ${wonBattles} won battles and ${energy} energy`
+            )
+            return
+        }
+
+        energy -= arr[i]
+        wonBattles += 1
+        if (wonBattles % 3 === 0) {
+            energy += wonBattles
+        }
+    }
+
+    console.log(`Won battles: ${wonBattles}. Energy left: ${energy}`)
+}
+
+// foo(["100", "10", "10", "10", "1", "2", "3", "73", "10"])
+foo(["200", "54", "14", "28", "13", "End of battle"])
