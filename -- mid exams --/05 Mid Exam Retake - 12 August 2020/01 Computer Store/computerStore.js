1ea3af68253804aa65a4121d9ca15ee76af6a3a7
Sineastra <lord.of.light.0002@gmail.com>
2020-10-28 16:03:00 +0200

    added  Mid Exam Retake - 12 August 2020 added 01 Computer Store

---
+++ -- mid exams --/05 Mid Exam Retake - 12 August 2020/01 Computer Store/computerStore.js
@@ -0,0 +1,31 @@
+function foo(arr) {
+    const customerIndex = arr.findIndex(x => x === "special" || x === "regular")
+    const workableArray = arr.slice(0, customerIndex)
+    let taxes = 0
+    let priceWithout = 0
+
+    for (let i = 0; i < workableArray.length; i++) {
+        if (workableArray[i] < 0) {
+            console.log(`Invalid price!`)
+            continue
+        }
+        taxes += workableArray[i] * 0.2
+        priceWithout += Number(workableArray[i])
+    }
+
+    let totalPrice = priceWithout + taxes
+
+    if (totalPrice === 0) {
+        console.log("Invalid order!")
+    } else {
+        if (arr[customerIndex] === "special") {
+            totalPrice *= 0.9
+        }
+
+        console.log(`Congratulations you've just bought a new computer!
+Price without taxes: ${priceWithout.toFixed(2)}$
+Taxes: ${taxes.toFixed(2)}$
+-----------
+Total price: ${totalPrice.toFixed(2)}$`)
+    }
+}
